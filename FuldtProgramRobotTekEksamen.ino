be4ed0f9ed753ed80084cf9c430baed182c3ccb4
Tobias-Henriksen <160583454+Tobias-Henriksen@users.noreply.github.com>
2026-04-21 16:39:23 +0200

    Added scan and sonar functions
    And switched libarary from  stepper to accelstepper

--- FuldtProgramRobotTekEksamen.ino
+++ FuldtProgramRobotTekEksamen.ino
@@ -1,11 +1,29 @@
+#include <AccelStepper.h>
 #include <Stepper.h>
 #include <Wire.h>
 
 int i;
 
-int knapPinX = 21;
-int knapPinY = 20;
-int knapPinZ = 19;
+const int numSamples = 5;
+int samples[numSamples];
+int sampleIndex = 0;
+
+unsigned long lastRead = 0;
+
+int stableCount = 0;
+int requiredStable = 3;
+int lastDistance = 0;
+int threshold = 4; // cm ændring før ændring bliver registreret som kasse kant
+
+const int sugePin = 3;
+
+const int trigPin = 10;
+const int echoPin = 8;
+
+
+const int knapPinX = 21;
+const int knapPinY = 20;
+const int knapPinZ = 19;
 bool knapStatus;
 
 int stepsX;
@@ -18,133 +36,251 @@ int distanceX;
 int distanceY;
 
 
-int error = 0;
-int step = 0;
-int taget = 0;
+// Motorhastighed i steps pr sekund. 1 rpm er 3,33..
+int MotorMaxSpeed1 = 700; 
+int MotorMaxSpeed2 = 700;
+int MotorMaxSpeed3 = 700;
 
-// Motorhastighed i rpm
-int MotorSpeed1 = 80; 
-int MotorSpeed2 = 80;
-int MotorSpeed3 = 80;
+// Motorhastighed acceleration
+int MotorAccel1 = 1500; 
+int MotorAccel2 = 1500;
+int MotorAccel3 = 1500;
+
 
 int stepsPerMM = 1; // 100 steps er 4,8 cm. 200 er en omgang
 
+AccelStepper xAkse(AccelStepper::FULL4WIRE, 53, 51, 50, 52);
+AccelStepper yAkse(AccelStepper::FULL4WIRE, 47, 45, 46, 44);
+AccelStepper zAkse(AccelStepper::FULL4WIRE, 38, 40, 41, 39);
 
-// Definer Stepper Motorer på akserne og sæt step rækkefølge.
-Stepper xAkse(200, 53, 51, 50, 52);
-Stepper yAkse(200, 47, 45, 46, 44);
-Stepper zAkse(200, 38, 40, 41, 39);
+//GAMMEL DEFINATION AF STEPPERE
+// // Definer Stepper Motorer på akserne og sæt step rækkefølge.
+
+// Stepper xAkse(200, 53, 51, 50, 52);
+// Stepper yAkse(200, 47, 45, 46, 44);
+// Stepper zAkse(200, 38, 40, 41, 39);
 
 
 
 void setup() {
   Serial.begin(9600);
 
+// Definer suge pin
+  pinMode(sugePin, OUTPUT);
+
+// Definer sonar pins.
+  pinMode(trigPin, OUTPUT); // Skal være PWM
+  pinMode(echoPin, INPUT);
+
+// Definer knap pins
   pinMode(knapPinX, INPUT_PULLUP);
   pinMode(knapPinY, INPUT_PULLUP);
   pinMode(knapPinZ, INPUT_PULLUP);
 
 
-  xAkse.setSpeed(MotorSpeed1);
-  yAkse.setSpeed(MotorSpeed2);
-  zAkse.setSpeed(MotorSpeed3);
+// Sæt motor hastighed i steps pr sekund. 1 rpm er 3,33..
+  xAkse.setMaxSpeed(MotorMaxSpeed1);
+  yAkse.setMaxSpeed(MotorMaxSpeed2);
+  zAkse.setMaxSpeed(MotorMaxSpeed3);
+
+// Sæt motor acceleration
+  xAkse.setAcceleration(MotorAccel1);
+  yAkse.setAcceleration(MotorAccel2);
+  zAkse.setAcceleration(MotorAccel3);
+
 
 // Kør kalibrering for hver akse.
- stepsX = calibrate("x");
- stepsY = calibrate("y");
+  stepsX = calibrate("x");
+  stepsY = calibrate("y");
+
+ 
 // stepsZ = calibrate("z");
 
 
-  move((20/4.85*100)*-1, -300); //Kør 20 cm (20/4.85*100)
+//  move(20/4.85*100, 50); //Kør 20 cm (20/4.85*100)
 
-  move(0,0);
+  // move(1200,800);
+
+  // move(0,0);
+
+  scanArea();
 
 
 
 }
 
 void loop() {
+//Serial.println(sonarInput());
 
 }
+
+
 
 
 int calibrate(int akse) {
 
-  while (true) {
-    // Læs hvilken knap der skal læses alt efter akse
-    if (akse == "x") {
-      knapStatus = digitalRead(knapPinX);
-    }
-    if (akse == "y") {
-      knapStatus = digitalRead(knapPinY);
-    }
-    if (akse == "z") {
-      knapStatus = digitalRead(knapPinZ);
-    }
+  AccelStepper* motor;
+  int pin;
 
-    // Hvis knappen er trykket er motoren i 0.
-    if (knapStatus == LOW) {
-      Serial.println("Stop!");
-      return 0; 
-    }
-
-    // Ellers fortsætter den til den rammer 0.
-    if (akse == "x") {
-      xAkse.step(stepsPerMM);  
-    }
-    if (akse == "y") {
-      yAkse.step(stepsPerMM);
-    }
-    if (akse == "z") {
-      zAkse.step(stepsPerMM);
-    }
+  if (akse == "x") {
+    motor = &xAkse;
+    pin = knapPinX;
   }
+  else if (akse == "y") {
+    motor = &yAkse;
+    pin = knapPinY;
+  }
+  else if (akse == "z") {
+    motor = &zAkse;
+    pin = knapPinZ;
+  }
+  else {
+    return -1; // fejl
+  }
+
+  motor->setMaxSpeed(600);
+  motor->setAcceleration(20000);
+
+  motor->moveTo(-10000);
+
+  // Kør mod home indtil knap rammes
+  while (digitalRead(pin) != LOW) {
+
+  
+    motor->run();
+  }
+
+  // Stop og nulstil position
+  motor->stop();
+  while (motor->isRunning()) {
+    motor->run();
+  }
+
+  motor->setCurrentPosition(0);
+
+  motor->setMaxSpeed(600);
+  motor->setAcceleration(1000);
+
+  return 0;
 }
-
-
-// int move(int stepsX,int targetX) {
-//   //tag targetX/Y og fratræk stepsX/Y. Resultat burde være antal steps til mål. Fortegn autoudregnet.
-
-//   distanceX = stepsX - targetX;
-// //  distanceY = stepsY - targetY;
-//   xAkse.step(distanceX);  
-//   stepsX = (stepsX + distanceX)*-1;
-//   Serial.println(stepsX);
-// //  yAkse.step(distanceY);
-//   return stepsX
-//   return stepsY
-
-// }
 
 void move(int targetX, int targetY) {
 
-  int distanceX = targetX - stepsX;  // hvor langt vi skal flytte os
+  xAkse.moveTo(targetX);
+  yAkse.moveTo(targetY);
 
-  int distanceY = targetY - stepsY;  // hvor langt vi skal flytte os
+  while (xAkse.distanceToGo() != 0 || yAkse.distanceToGo() != 0) {
+    xAkse.run();
+    yAkse.run();
 
-
-  int directionX = (distanceX > 0) ? 1 : -1;
-
-  for (int i = 0; i < abs(distanceX); i++) {
-    xAkse.step(directionX);
   }
 
-  
-  int directionY = (distanceY > 0) ? 1 : -1;
-
-  for (int i = 0; i < abs(distanceY); i++) {
-    yAkse.step(directionY);
-  }
-
+  stepsX = targetX;
   stepsY = targetY;
-
-  stepsX = targetX;  // opdater position korrekt
 
   Serial.print("Ny position: ");
   Serial.print(stepsY);
   Serial.print("; ");
   Serial.println(stepsX);
 }
+
+
+
+
+
+long sonarInput() {
+
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  long duration = pulseIn(echoPin, HIGH, 30000); // timeout 30ms
+
+  if (duration == 0) return -1;
+
+  long distance = duration * 0.034 / 2;
+
+  if (distance < 2 || distance > 400) return -1; // filtrer ukorrekte målinger fra
+
+  return distance;
+
+
+}
+
+void sonarAverage(){ // Gem koordinator (stepsX/Y) når en ændring er dedekteret. 
+
+  // Begræns målefrekvens
+  if (millis() - lastRead < 50) return;
+  lastRead = millis();
+
+  int distance = sonarInput();
+
+  if (distance == -1) return;
+
+  // Gem i buffer
+  samples[sampleIndex] = distance;
+  sampleIndex = (sampleIndex + 1) % numSamples;
+
+  // Beregn gennemsnit
+  int sum = 0;
+  for (int i = 0; i < numSamples; i++) {
+    sum += samples[i];
+  }
+  int filtered = sum / numSamples;
+
+  // Stabil ændring (anti-støj)
+  if (abs(filtered - lastDistance) > threshold) {
+    stableCount++;
+  } else {
+    stableCount = 0;
+  }
+
+  if (stableCount >= requiredStable) {
+
+    Serial.println("Kant fundet!");
+
+    Serial.print("Position X: ");
+    Serial.print(stepsX);
+    Serial.print(" Y: ");
+    Serial.println(stepsY);
+
+    stableCount = 0;
+  }
+
+  lastDistance = filtered;
+}
+
+
+
+void scanArea() {
+  int x = 400;
+  int y = 400; 
+
+  int scanXStep = 10;
+  int scanYStep = 100; // Afstand kørt på y efter hvert fuldførte x akse tur
+
+
+for (int y = 0; y <= 400; y += scanYStep) {
+  for (int x = 0; x <= 400; x += scanXStep) {
+
+    xAkse.moveTo(x);
+    while (x != xAkse.currentPosition()) {
+      xAkse.run();
+      Serial.println(sonarInput());   
+    }
+ 
+
+    yAkse.moveTo(y);
+    while (y != yAkse.currentPosition()) {
+      yAkse.run();
+    }
+  }
+}
+
+  }
 
 
 
